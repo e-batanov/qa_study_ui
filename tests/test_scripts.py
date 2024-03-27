bc14a5bc2e666da00d41feae805ab9d970a034f7
e-batanov <144840859+e-batanov@users.noreply.github.com>
2024-03-27 21:02:13 +0300

    Uncommented the tests

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -2,35 +2,35 @@ from tests.common import assert_element
 from selenium.webdriver.common.by import By
 
 
-# def test_auth_admin(browser, url):
-#     browser.get(f'{url}/administration')
-#
-#     assert_element(browser, (By.XPATH, '//*[@class="card"]'))
-#     user = browser.find_element(By.XPATH, '//*[@id="input-username"]')
-#     user.send_keys('user')
-#     password = browser.find_element(By.XPATH, '//*[@id="input-password"]')
-#     password.send_keys('bitnami')
-#     browser.find_element(By.XPATH, '//*[@class="btn btn-primary"]').click()
-#     assert_element(browser, (By.XPATH, '//*[@id="nav-profile"]'))
-#     log_out = assert_element(browser, (By.XPATH, '//*[@id="nav-logout"]'))
-#     log_out.click()
-#
-#
-# def test_user_flow(browser, url):
-#     browser.get(url)
-#
-#     assert_element(browser, (By.XPATH, '//*[@id="logo"]'))
-#     card_header = assert_element(browser, (By.XPATH, '//*[@id="header-cart"]'))
-#     card_header.click()
-#     card_header = browser.find_element(By.XPATH, "//*[text()='Your shopping cart is empty!']")
-#
-#     card_header.click()
-#     add_to_cart_buttons = browser.find_element(By.XPATH, '//*[@class="fa-solid fa-shopping-cart"]')
-#     add_to_cart_buttons.click()
-#
-#     card_header = assert_element(browser, (By.XPATH, '//*[@id="header-cart"]'))
-#     card_header.click()
-#     assert not card_header.text == 'Your shopping cart is empty!'
+def test_auth_admin(browser, url):
+    browser.get(f'{url}/administration')
+
+    assert_element(browser, (By.XPATH, '//*[@class="card"]'))
+    user = browser.find_element(By.XPATH, '//*[@id="input-username"]')
+    user.send_keys('user')
+    password = browser.find_element(By.XPATH, '//*[@id="input-password"]')
+    password.send_keys('bitnami')
+    browser.find_element(By.XPATH, '//*[@class="btn btn-primary"]').click()
+    assert_element(browser, (By.XPATH, '//*[@id="nav-profile"]'))
+    log_out = assert_element(browser, (By.XPATH, '//*[@id="nav-logout"]'))
+    log_out.click()
+
+
+def test_user_flow(browser, url):
+    browser.get(url)
+
+    assert_element(browser, (By.XPATH, '//*[@id="logo"]'))
+    card_header = assert_element(browser, (By.XPATH, '//*[@id="header-cart"]'))
+    card_header.click()
+    card_header = browser.find_element(By.XPATH, "//*[text()='Your shopping cart is empty!']")
+
+    card_header.click()
+    add_to_cart_buttons = browser.find_element(By.XPATH, '//*[@class="fa-solid fa-shopping-cart"]')
+    add_to_cart_buttons.click()
+
+    card_header = assert_element(browser, (By.XPATH, '//*[@id="header-cart"]'))
+    card_header.click()
+    assert not card_header.text == 'Your shopping cart is empty!'
 
 
 def test_check_main_price(browser, url):
